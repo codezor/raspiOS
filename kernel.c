afcd2143e1bca2680d355d75fc4d341579867d23
codezor <codezor4@gmail.com>
2019-03-07 16:27:54 -0500

    fixes kernel.c

--- kernel.c
+++ kernel.c
@@ -13,7 +13,7 @@ static inline void mmio_write(uint32_t reg, uint32_t data)
 }
 
 // Memory Map read the IO on the Rasberry PI
-static inline uint32_t mmioread(uint32_t reg)
+static inline uint32_t mmio_read(uint32_t reg)
 {
     return *(volatile uint32_t*)reg;
 }
